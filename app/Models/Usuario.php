4f9877f66536a1890756e6b71aa535f0b332b842
Brenda Lopez <72949449+brenlopez@users.noreply.github.com>
2023-04-08 23:31:02 -0300

    Modelos de Usuario y Roles

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+
+    protected $table = 'usuarios';
+    protected $primaryKey = 'usuario_id';
+
+    protected $fillable = ['nombre', 'apellido', 'dni', 'email','password','foto_perfil','rol_id'];
+
+    public function roles()
+    {
+        return $this->belongsTo('App\Models\Rol','rol_id');
+    }
 }
